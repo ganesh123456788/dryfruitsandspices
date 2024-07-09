a61c2abdb945acebaf3a63e4096476af7a52b342
ganesh123456788 <yanagandlaganesh@gmail.com>
2024-07-09 10:14:30 +0530

    09-07-24

---
+++ WebApplication8/WebApplication8/Controllers/HomeController.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult ddryfruitsandspices()
+        {
+            return View();
+        }
+        public ActionResult stock2()
+        {
+            return View();
+        }
+        public ActionResult stock()
+        {
+            return View();
+        }
+        public ActionResult dryfruits()
+        {
+            return View();
+        }
+        public ActionResult cchocolatewithalmond()
+        {
+            return View();
+        }
+        public ActionResult chazelnutmilkchocolate()
+        {
+            return View();
+        }
+        public ActionResult ccoconutchocolate()
+        {
+            return View();
+        }
+        public ActionResult cdateschocolate()
+        {
+            return View();
+        }
+        public ActionResult spices()
+        {
+            return View();
+        }
+        public ActionResult DRYFRUITSCHOCOLATES()
+        {
+            return View();
+        }
+        public ActionResult SAVEPAGE()
+        {
+            return View();
+        }
+        public ActionResult SAVEPAGE3()
+        {
+            return View();
+        }
+        public ActionResult ADDCHOCOLATES()
+        {
+            return View();
+        }
+        public ActionResult adddryfruits()
+        {
+            return View();
+        }
+        public ActionResult EDITPAGE()
+        {
+            return View();
+        }
+        public ActionResult delete()
+        {
+            return View();
+        }
+        public ActionResult ccashews()
+        {
+            return View();
+        }
+        public ActionResult calmonds()
+        {
+            return View();
+        }
+        public ActionResult cdriedplums()
+        {
+            return View();
+        }
+        public ActionResult cdriedanjeer()
+        {
+            return View();
+        }
+        public ActionResult cdriedstrawberries()
+        {
+            return View();
+        }
+        public ActionResult capricot()
+        {
+            return View();
+        }
+        public ActionResult cdriedkiwi()
+        {
+            return View();
+        }
+        public ActionResult cdriedamla()
+        {
+            return View();
+        }
+        public ActionResult cdriedcherry()
+        {
+            return View();
+        }
+        public ActionResult cdriedbluberries()
+        {
+            return View();
+        }
+        public ActionResult csaltedcashews()
+        {
+            return View();
+        }
+        public ActionResult cdriedcranberries()
+        {
+            return View();
+        }
+        public ActionResult cpistachios()
+        {
+            return View();
+        }
+        public ActionResult cdriedblackberry()
+        {
+            return View();
+        }
+        public ActionResult cturmericpowder()
+        {
+            return View();
+        }
+        public ActionResult celachi()
+        {
+            return View();
+        }
+        public ActionResult craisins()
+        {
+            return View();
+        }
+        public ActionResult cdates()
+        {
+            return View();
+        }
+        public ActionResult cdriedoranges()
+        {
+            return View();
+        }
+        public ActionResult cblackpepper()
+        {
+            return View();
+        }
+        public ActionResult cmustardseeds()
+        {
+            return View();
+        }
+        public ActionResult ccloves()
+        {
+            return View();
+        }
+        public ActionResult ccinnamon()
+        {
+            return View();
+        }
+        public ActionResult ccoriander()
+        {
+            return View();
+        }
+        public ActionResult ccuminseeds()
+        {
+            return View();
+        }
+        public ActionResult cbayleaf()
+        {
+            return View();
+        }
+        public ActionResult cyellowmustard()
+        {
+            return View();
+        }
+        public ActionResult cnutmeg()
+        {
+            return View();
+        }
+        public ActionResult cmace()
+        {
+            return View();
+        }
+        public ActionResult cstaranice()
+        {
+            return View();
+        }
+        public ActionResult cchillipowder()
+        {
+            return View();
+        }
+        public ActionResult savepage2()
+        {
+            return View();
+        }
+        public ActionResult ADDSPICES()
+        {
+            return View();
+        }
+        public ActionResult csaffron()
+        {
+            return View();
+        }
+        public ActionResult cdriedmango()
+        {
+            return View();
+        }
+        public ActionResult profile()
+        {
+            return View();
+        }
+        public ActionResult settings()
+        {
+            return View();
+        }
+        public ActionResult adminpage()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
